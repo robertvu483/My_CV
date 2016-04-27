c8660410a7073182692876b0e978116251fab296
Vũ Hoàng Phúc <ryanvu644@gmail.com>
2016-04-27 19:05:20 +0700

    Raw Content

--- app/app.js
+++ app/app.js
@@ -1,4 +1,32 @@
 /*global angular*/
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', []);
+var myCV = angular.module('myCV', []);
+
+myCV.controller('bodyController', function ($scope, $http) {
+    'use strict';
+    
+    $http.get("personal_info/basic.json").success(function (data) {
+        $scope.basic = data;
+    });
+    
+    $http.get("personal_info/summary.json").success(function (data) {
+        $scope.summary = data;
+    });
+    
+    $http.get("personal_info/experience.json").success(function (data) {
+        $scope.exp = data;
+    });
+    
+    $http.get("personal_info/projects.json").success(function (data) {
+        $scope.prj = data;
+    });
+    
+    $http.get("personal_info/skills.json").success(function (data) {
+        $scope.skills = data;
+    });
+    
+    $http.get("personal_info/education.json").success(function (data) {
+        $scope.education = data;
+    });
+});
