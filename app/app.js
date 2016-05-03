efd284de070379414594da33ce21512d9f2e9d0c
Vũ Hoàng Phúc <ryanvu644@gmail.com>
2016-05-03 21:42:21 +0700

    Editable: Experience

--- app/app.js
+++ app/app.js
@@ -18,6 +18,14 @@ myCV.controller('bodyController', function ($scope, $http) {
     
     $http.get("personal_info/experience.json").success(function (data) {
         $scope.exp = data;
+        
+        var experiences = $scope.exp.experiences,
+            i;
+        
+        for (i = 0; i < experiences.length; i = i + 1) {
+            experiences[i].startDate = new Date(experiences[i].startDate);
+            experiences[i].endDate = new Date(experiences[i].endDate);
+        }
     });
     
     $http.get("personal_info/projects.json").success(function (data) {
@@ -32,33 +40,64 @@ myCV.controller('bodyController', function ($scope, $http) {
         $scope.education = data;
     });
     
-    $scope.show = {
+    $scope.display = {
         "basicName": true,
         "basicHeadline": true,
         "basicLocation": true,
         "basicContact": true,
-        "summary": true
+        "summary": true,
+        "experience": true
     };
     
     $scope.temp = {
         "basicName": "",
         "basicHeadline": "",
         "basicLocation": "",
-        "basicContact": "",
+        "basicContact": {},
         "summary": "",
+        "experience": {}
     };
     
-    $scope.toggle = function (showName) {
-        $scope.show[showName] = !$scope.show[showName];
+    $scope.editIndex = {
+        "experience": -1
     };
     
-    $scope.editForm = function (showName, curValue) {
-        $scope.toggle(showName);
-        $scope.temp[showName] = cloneObject(curValue);
+    $scope.show = function (displayName) {
+        $scope.display[displayName] = true;
     };
     
-    $scope.submitForm = function (showName, obj, attrName, newValue) {
-        $scope.toggle(showName);
+    $scope.hide = function (displayName) {
+        $scope.display[displayName] = false;
+    };
+    
+    $scope.editForm = function (displayName, curValue) {
+        $scope.hide(displayName);
+        $scope.temp[displayName] = cloneObject(curValue);
+    };
+    
+    $scope.editArrayForm = function (displayName, curValue, index) {
+        $scope.editIndex[displayName] = index;
+        $scope.editForm(displayName, curValue);
+    };
+    
+    $scope.submitForm = function (displayName, obj, attrName, newValue) {
+        $scope.show(displayName);
         obj[attrName] = cloneObject(newValue);
+    };
+    
+    $scope.submitArrayForm = function (displayName, array, newValue) {
+        var index = $scope.editIndex[displayName];
+        
+        $scope.show(displayName);
+        
+        if (index !== -1) {
+            array[index] = cloneObject(newValue);
+        } else {
+            array.push(cloneObject(newValue));
+        }
+    };
+    
+    $scope.hasData = function (obj) {
+        return !(typeof obj === "undefined" || obj === null || obj === "");
     };
 });
--- app/components/usefulFunction/functions.js
+++ app/components/usefulFunction/functions.js
@@ -1,15 +1,20 @@
 function cloneObject(obj) {
     'use strict';
     
-    var keys = Object.keys(obj),
+    var keys,
         temp,
         i;
     
     if (obj === null || typeof obj !== 'object') {
         return obj;
     }
- 
+    
+    if (obj instanceof Date) {
+        return new Date(obj.getTime());
+    }
+    
     temp = obj.constructor(); // give temp the original obj's constructor
+    keys = Object.keys(obj)
     for (i = 0; i < keys.length; i = i + 1) {
         temp[keys[i]] = cloneObject(obj[keys[i]]);
     }
